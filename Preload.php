37799e23ee13e4fc22617680675c68aed0d22347
TomasHelby <valientin.rudin@mail.ru>
2024-06-05 18:52:34 +0500

    Выполнено

---
+++ Preload.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            <?= $title ?>
+        </title>
+    </head>
+    <body>
+        <h1> <?= $h1 ?> </h1>
+        <div>
+            <?= $year ?>
+        </div>
+        <h2> <?= $h2 ?> </h2>
+        <div>
+             <?= getStringTime() ?>
+        </div>
+    </body>
+</html>
